0b9bf993343d195b24d4a874f7cc279d4c56448e
wadew00 <vedatbasbugu@gmail.com>
2026-03-03 22:23:58 +0300

    File structure changed so that everything in the header file and main.cpp as a test tool

--- Vector.h
+++ Vector.h
@@ -1,21 +1,70 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 #include <cstddef>
+template <typename T>
 class Vector
 {
-public:
-  Vector();
-  Vector(std::size_t s);
-  Vector(const Vector &v);
-  Vector &operator=(const Vector &v);
-  Vector(Vector &&v);
-  Vector &operator=(Vector &&v);
-  ~Vector();
-  double operator[](std::size_t s) const;
-  double &operator[](std::size_t s);
-  std::size_t size() const;
+private:
+  std::size_t sz;
+  T *arr;
+  void swap(Vector &v)
+  {
+    std::swap(sz, v.sz);
+    std::swap(arr, v.arr);
+  }
 
-  double *begin();
-  double *end();
+public:
+  // constructor
+  Vector() : sz{0}, arr{nullptr} {};
+  Vector(std::size_t s) : sz{s}, arr{new double[s]} {};
+  // copy constructor
+  // (with constructor delegation)
+  Vector(const Vector &v) : Vector(v.sz)
+  {
+    std::copy(v.arr, v.arr + v.sz, arr);
+  }
+  // assignment operator
+  // (with copy-swap)
+  Vector &operator=(const Vector &v)
+  {
+    Vector cpy(v);
+    swap(cpy);
+    return *this;
+  }
+  // move constructor
+  Vector(Vector &&v) : Vector()
+  {
+    swap(v);
+  }
+  // move assignment
+  Vector &operator=(Vector &&v)
+  {
+    swap(v);
+    return *this;
+  }
+  // destructor
+  ~Vector() { delete[] arr; }
+  // GETTER and SETTER
+  double operator[](std::size_t s) const
+  {
+    return arr[s];
+  }
+  double &operator[](std::size_t s)
+  {
+    return arr[s];
+  }
+  std::size_t size() const
+  {
+    return sz;
+  }
+  // Iterator
+  double *begin()
+  {
+    return arr;
+  }
+  double *end()
+  {
+    arr + sz;
+  }
 };
 #endif
